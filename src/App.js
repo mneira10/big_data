4fd7cfd66342d52435e448eac733c98308b0f8d0
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-02-04 16:41:28 -0500

    Trigger rebuild

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
 import './App.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Taller1 from './components/taller1/Taller1';
+import Taller2 from './components/taller2/Taller2';
+import Taller3 from './components/taller3/Taller3';
+
+//color palette 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#448ef6',
+    accent1Color: '#f16821',
+  },
+});
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 >Big Data</h1>
+        <h2 >Grupo 7</h2>
+        
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <Tabs>
+            <Tab label="Taller 1" >
+              <div class="taller">
+                <Taller1/>
+              </div>
+            </Tab>
+            <Tab label="Taller 2" >
+              <div class="taller">
+                <Taller2/>
+              </div>
+            </Tab>
+            <Tab label="Taller 3">
+              <div class="taller">
+                <Taller3/>
+              </div>
+            </Tab>
+          </Tabs>
+        </MuiThemeProvider>
+
       </div>
     );
   }
---
+++ src/components/taller1/Taller1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Taller1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Taller 1</h1>
+
+        <p>//TODO</p>
+        <p>Aquí va a ir todo lo del primer taller. Imagensita:</p>
+
+        <img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Social_Network_Analysis_Visualization.png'></img>
+      </div>
+    )
+  }
+}
